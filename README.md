a84598ba0856226b8c6ebb14e6d45e169b0df98b
Julio Ribeiro <jcbr.vet@gmail.com>
2021-04-05 21:35:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+##Super Trunfo Dogs
+
+<p>É um jogo de cartas com a temática raças de cães.</p>
+
+<p>No início do jogo 24 cartas são sorteadas e distribuídas. A cada rodada o jogador recebe uma carta com 4 atributos.</p>
+
+<p>O jogador deve escolher um atributo da sua carta para tentar vencer o atributo da carta da máquina.</p>
+
+<p> Ao fim de cada rodada é somado um ponto pro ganhador. </p>
+
+<p>O jogo termina quando acabam as cartas.</p>
+
+<br>
+<p>Level: Beginner</p>
+<p>Languages: <img align="center" alt="Rafa-HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+  <img align="center" alt="Rafa-CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+   <img align="center" alt="Rafa-Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+</p>
+
---
+++ script.js
@@ -0,0 +1,477 @@
+var cartaPinscher = {
+    nome: "Pinscher",
+    imagem: 'https://www.adoropets.com.br/wp-content/uploads/2019/11/chihuahua-bravo.jpg',
+    atributos: {
+        peso: 4,
+        agilidade: 9,
+        obediência: 1,
+        agressividade: 10
+
+    }
+}
+
+var cartaBeagle = {
+    nome: "Beagle",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/40f228f4cc8ea770ff4a83d1dd4aec59_breed.jpg",
+    atributos: {
+        peso: 11,
+        agilidade: 8,
+        obediência: 9,
+        agressividade: 3
+    }
+}
+
+var cartaAmerican = {
+    nome: "American Bully",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/3606fdbc2efcd0b5253793140ba00d78_breed.jpg",
+    atributos: {
+        peso: 40,
+        agilidade: 7,
+        obediência: 7,
+        agressividade: 7
+    }
+}
+
+var cartaPitBull = {
+    nome: "Pit Bull",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/8e556ccb4f1c43566653a7e1a4cdc719_breed.jpg",
+    atributos: {
+        peso: 22,
+        agilidade: 8,
+        obediência: 7,
+        agressividade: 8
+    }
+}
+
+var cartaBasset = {
+    nome: "Basset Hound",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/70f29ffb6feebad4e5def55e1d6d58e6_breed.jpg",
+    atributos: {
+        peso: 26,
+        agilidade: 5,
+        obediência: 3,
+        agressividade: 2
+    }
+}
+
+var cartaBernese = {
+    nome: "Bernese",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/f0558e34c17546ab72ad84ab08500c17_breed.jpg",
+    atributos: {
+        peso: 44,
+        agilidade: 6,
+        obediência: 8,
+        agressividade: 2
+    }
+}
+
+var cartaBorderCollie = {
+    nome: "Border Collie",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/7a189a97ec2b16026eb2455672fbeb9f_breed.jpg",
+    atributos: {
+        peso: 20,
+        agilidade: 9,
+        obediência: 10,
+        agressividade: 3
+    }
+}
+
+var cartaBullDogFrances = {
+    nome: "Buldogue Francês",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/e35db1cecea61c3ddba05efb5dec004e_breed.jpg",
+    atributos: {
+        peso: 10,
+        agilidade: 5,
+        obediência: 5,
+        agressividade: 3
+    }
+}
+
+var cartaBullDogIngles = {
+    nome: "Bulldog Inglês",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/df363ad100d38e6975a92210d421c4e5_breed.jpg",
+    atributos: {
+        peso: 23,
+        agilidade: 2,
+        obediência: 3,
+        agressividade: 3
+    }
+}
+
+var cartaBoxer = {
+    nome: "Bóxer",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/f9ccbfcaa19023977ec779b370a5fc8a_breed.jpg",
+    atributos: {
+        peso: 30,
+        agilidade: 8,
+        obediência: 6,
+        agressividade: 5
+    }
+}
+
+var cartaChowChow = {
+    nome: "Chow Chow",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/ff70feb5a253a698bee2c2c10943270c_breed.jpg",
+    atributos: {
+        peso: 27,
+        agilidade: 6,
+        obediência: 5,
+        agressividade: 8
+    }
+}
+
+var cartaAkita = {
+    nome: "Akita",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/4f62a367df378a3fbd25173f6126eb25_breed.jpg",
+    atributos: {
+        peso: 40,
+        agilidade: 5,
+        obediência: 5,
+        agressividade: 8
+    }
+}
+
+var cartaDalmata = {
+    nome: "Dálmata",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/071a70786b76233baf85a145d3c9d623_breed.jpg",
+    atributos: {
+        peso: 28,
+        agilidade: 8,
+        obediência: 7,
+        agressividade: 7
+    }
+}
+
+var cartaGalgo = {
+    nome: "Galgo",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/95509c361ac79590907265dfdf8cb755_breed.jpg",
+    atributos: {
+        peso: 25,
+        agilidade: 8,
+        obediência: 8,
+        agressividade: 6
+    }
+}
+
+var cartaLabrador = {
+    nome: "Labrador",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/b265eb8b6feb028680a2cf7fce991ce0_breed.jpg",
+    atributos: {
+        peso: 32,
+        agilidade: 9,
+        obediência: 9,
+        agressividade: 3
+    }
+}
+
+var cartaPastor = {
+    nome: "Pastor-Alemão",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/717f24f989f44532c715cc35cd6564a4_breed.jpg",
+    atributos: {
+        peso: 30,
+        agilidade: 8,
+        obediência: 9,
+        agressividade: 7
+    }
+}
+
+var cartaPequines = {
+    nome: "Pequinês",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/7e19fbe758e2848f5c84dbcdd740c4b6_breed.jpg",
+    atributos: {
+        peso: 4,
+        agilidade: 2,
+        obediência: 2,
+        agressividade: 2
+    }
+}
+
+var cartaPug = {
+    nome: "Pug",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/4cd7b243b1b0cd8a9c73ad9ccac98dba_breed.jpg",
+    atributos: {
+        peso: 7,
+        agilidade: 3,
+        obediência: 3,
+        agressividade: 4
+    }
+}
+
+var cartaRottweiler = {
+    nome: "Rottweiler",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/90496553c793a3166f43f5ea55bf1a0d_breed.jpg",
+    atributos: {
+        peso: 45,
+        agilidade: 9,
+        obediência: 8,
+        agressividade: 9
+    }
+}
+
+var cartaSpitz = {
+    nome: "Spitz Alemão",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/dc18c063058fff926583f46b2a2b4dd0_breed.jpg",
+    atributos: {
+        peso: 6,
+        agilidade: 5,
+        obediência: 7,
+        agressividade: 5
+    }
+}
+
+var cartaSaoBernardo = {
+    nome: "São Bernardo",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/8eac25624d14021a636c40d0ccc856f6_breed.jpg",
+    atributos: {
+        peso: 70,
+        agilidade: 6,
+        obediência: 7,
+        agressividade: 5
+    }
+}
+
+var cartaYork = {
+    nome: "Yorkshire",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/0164d3140f8b526dab6dea5ed5c42406_breed.jpg",
+    atributos: {
+        peso: 3,
+        agilidade: 4,
+        obediência: 6,
+        agressividade: 6
+    }
+}
+
+var cartaPoodle = {
+    nome: "Poodle",
+    imagem: "https://vetsmart-parsefiles.s3.amazonaws.com/eed7613e28ab15a3263e2bffc0328795_breed.jpg",
+    atributos: {
+        peso: 8,
+        agilidade: 6,
+        obediência: 7,
+        agressividade: 5
+    }
+}
+
+var cartaSRD = {
+    nome: "Vira-Lata Caramelo",
+    imagem: "https://img.ibxk.com.br/2020/08/07/07115937437113.jpg?w=328",
+    atributos: {
+        peso: 100,
+        agilidade: 10,
+        obediência: 10,
+        agressividade: 10
+    }
+}
+
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaPinscher, cartaBeagle, cartaAmerican, cartaPitBull, cartaBasset, cartaBernese, cartaBorderCollie, cartaBullDogFrances, cartaBullDogIngles, cartaBoxer, cartaChowChow, cartaAkita, cartaDalmata, cartaGalgo, cartaLabrador, cartaPastor, cartaPequines, cartaPug, cartaRottweiler, cartaSpitz, cartaSaoBernardo, cartaYork,cartaPoodle, cartaSRD]
+
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+function atualizaQuantidadeDeCartas() {
+    var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+    var html = `Quantidade de cartas no jogo: ${cartas.length}`
+    divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar() {
+    var divPlacar = document.getElementById('placar')
+    var html = `Jogador ${pontosJogador} / ${pontosMaquina} Máquina`
+    divPlacar.innerHTML = html
+}
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
+
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    cartaJogador = cartas[numeroCartaJogador]
+    console.log(cartaJogador.nome)
+    cartas.splice(numeroCartaJogador, 1)
+
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+
+
+    document.querySelector('.wrapper').scrollIntoView({
+        behavior: 'smooth'
+    });
+
+    // //======SUPER TRUNFO=====
+    if (cartaJogador.nome == "Vira-Lata Caramelo") {
+        var divResultado = document.getElementById('resultado')
+        htmlResultado = `<p class="resultado-final">SUPER TRUNFO!!Você venceu!</p>`
+        divResultado.innerHTML = htmlResultado
+        document.getElementById('btnJogar').disabled = true
+        document.getElementById('btnProximaRodada').disabled = false
+        document.getElementById("somGanhou").play();
+        pontosJogador++
+        atualizaPlacar()
+        exibeCartaMaquina()
+        atualizaQuantidadeDeCartas()
+        document.querySelector('#carta-jogador').scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    }
+    if (cartaMaquina.nome == "Vira-Lata Caramelo") {
+        var divResultado = document.getElementById('resultado')
+        htmlResultado = `<p class="resultado-final">SUPER TRUNFO!!Você perdeu!</p>`
+        divResultado.innerHTML = htmlResultado
+        document.getElementById('btnJogar').disabled = true
+        document.getElementById('btnProximaRodada').disabled = false
+        document.getElementById("somPerdeu").play();
+        pontosMaquina++
+        exibeCartaMaquina()
+        atualizaPlacar()
+        atualizaQuantidadeDeCartas()
+        document.querySelector('#carta-jogador').scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    }
+
+    exibeCartaJogador()
+
+}
+
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById('carta-jogador')
+
+    var moldura = `<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">`
+
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+
+    opcoesTexto = ""
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = `<div id="opcoes" class="carta-status">`
+
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById('carta-maquina')
+
+    var moldura = `<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">`
+
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+
+    opcoesTexto = ""
+    for (var atributo in cartaMaquina.atributos) {
+        opcoesTexto += `<p type='text' name='atributo' value='" + atributo + "'>` + atributo + " " + cartaMaquina.atributos[atributo] + `</p>`
+    }
+
+    var html = `<div id="opcoes" class="carta-status --spacing">`
+
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+
+}
+
+function jogar() {
+    var divResultado = document.getElementById('resultado')
+    var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (document.querySelectorAll('input[type="radio"]:checked').length === 0) {
+        alert("Por favor, selecione um atributo.")
+        return
+    }
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = `<p class="resultado-final">Você venceu!</p>`
+        document.getElementById("somGanhou").play();
+        pontosJogador++
+    }
+
+    else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = `<p class="resultado-final">Você perdeu!</p>`
+        document.getElementById("somPerdeu").play();
+        pontosMaquina++
+    }
+
+    else {
+        htmlResultado = `<p class="resultado-final">Empatou!</p>`
+        document.getElementById("somEmpatou").play();
+    }
+
+    if (cartas.length == 0) {
+        alert('Fim de jogo')
+
+        if (pontosJogador > pontosMaquina) {
+            htmlResultado = '<p class="resultado-final">Venceu o jogo!</p>'
+        }
+
+        else if (pontosMaquina > pontosJogador) {
+            htmlResultado = '<p class="resultado-final">Perdeu o jogo!</p>'
+        }
+        else {
+            htmlResultado = '<p class="resultado-final">O jogo empatou!</p>'
+
+        }
+    }
+
+    else {
+        document.getElementById('btnProximaRodada').disabled = false
+    }
+
+    divResultado.innerHTML = htmlResultado
+    document.getElementById('btnJogar').disabled = true
+
+    document.querySelector('#carta-jogador').scrollIntoView({
+        behavior: 'smooth'
+    });
+
+    atualizaPlacar()
+    exibeCartaMaquina()
+    atualizaQuantidadeDeCartas()
+}
+
+function proximaRodada() {
+    var divCartas = document.getElementById('cartas')
+    divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div><div id="carta-maquina" class="carta"></div>`
+
+    document.getElementById('btnSortear').disabled = false
+    document.getElementById('btnJogar').disabled = true
+    document.getElementById('btnProximaRodada').disabled = true
+
+    var divResultado = document.getElementById('resultado')
+    divResultado.innerHTML = ""
+
+    document.querySelector('.page-logo').scrollIntoView({
+        behavior: 'smooth'
+    });
+
+}
+
+
+
+
+
